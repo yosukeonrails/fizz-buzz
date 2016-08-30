1f8e12b957ad5653fb183255a269fae179833f62
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-08-30 00:05:44 -0700

    inputfield version

--- js/app.js
+++ js/app.js
@@ -3,15 +3,58 @@
 	$(document).ready(function(){
 
 
+var clickCount=0
+
+    $(".submit-button").click(function(){
+
+		var inputed_number = parseInt($(".fizzbuzz-input").val()) 
+
+		if(Number.isInteger(inputed_number)){
+			
+			fizzBuzzCounter(inputed_number);
+
+		} else {alert('Not a number')}
+
+			clickCount=clickCount+1
+
+			console.log(clickCount)
+
+			// if(clickCount==2){
+
+			
+			// }
+
+
+});
+
+
+   
+      
+
+	// $(".submit-button").click(function(){
+
+	// 	if(inputed_number % 1 != 0){
+	// 		alert('not a whole number! please input whole number')
+	// 	} else {
+
+
+	// 		console.log('yep it went well'+inputed_number)
+	// 	}
 		
-		fizzBuzzCounter();
+
+	// })
+		
+		
 
 
 	})
 
-	function fizzBuzzCounter(){
+
+
+	function fizzBuzzCounter(numberInputed){
 
     var i=0
+    var i=numberInputed
 
 		while(i<100){
 
